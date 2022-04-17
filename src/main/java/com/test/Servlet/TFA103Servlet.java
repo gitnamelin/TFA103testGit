9e5aa7a0cdb99158b8050237b0c8c72fa8f9ccb7
jorge <s76563100@gmail.com>
2022-04-17 21:36:00 +0800

    added doget

--- src/main/java/com/test/Servlet/TFA103Servlet.java
+++ src/main/java/com/test/Servlet/TFA103Servlet.java
@@ -14,5 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 public class TFA103Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
 }
